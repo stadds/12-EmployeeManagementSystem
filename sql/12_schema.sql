eeb7fc0d9a6d2dcd4c93e6aeb7e9169cbf77a242
STadds <staddi@gmail.com>
2020-04-29 21:18:04 -0400

    update scheme for UPDATES/DELETES

--- sql/12_schema.sql
+++ sql/12_schema.sql
@@ -6,15 +6,15 @@ USE employee_mgmtDB;
 
 CREATE TABLE department(
 	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(30),
+	name VARCHAR(30) NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE emp_role(
 	id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL(10,2),
-    department_id INT NOT NULL,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10,2) NULL,
+    department_id INT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 		 ON UPDATE CASCADE ON DELETE SET NULL 
@@ -24,8 +24,8 @@ CREATE TABlE employee(
 	id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NULL,
     last_name VARCHAR(30) NULL,
-    role_id INT NOT NULL ,
-    manager_id INT ,    
+    role_id INT NULL ,
+    manager_id INT NULL,    
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES emp_role(id)  ON UPDATE CASCADE ON DELETE SET NULL ,
     FOREIGN KEY (manager_id) REFERENCES employee(id)  ON UPDATE CASCADE ON DELETE SET NULL 
